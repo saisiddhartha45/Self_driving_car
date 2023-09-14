96ebbbdec50aad449043b8ad505d3fea8281c501
Windows <saisiddhartha45@gmial.com>
2023-09-14 08:56:51 +0530

    added road and lanes

--- car.js
+++ car.js
@@ -6,32 +6,68 @@ class Car {
         this.height = height;
 
         this.controls = new Controls();
+
+        this.speed = 0;
+        this.accelecration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
     }
     update(){
-        if(this.controls.forward){
-            this.y-=2;
-        }
-        if(this.controls.reverse){
-            this.y+=2;
-        }
-        if(this.controls.left){
-            this.x-=2;
-        }
-        if(this.controls.right){
-            this.x+=2;
-        }
-
+        this.#move()
 
     }
+    #move(){
+        if(this.controls.forward){
+            this.speed += this.accelecration;
+        }
+        if(this.controls.reverse){
+            this.speed -= this.accelecration;
+        }
+
+        if(this.speed > this.maxSpeed){
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed <- this.maxSpeed/2){
+            this.speed =-this.maxSpeed/2
+        }
+        if(this.speed > 0){
+            this.speed -= this.friction;
+        }
+        if(this.speed < 0){
+            this.speed += this.friction;
+        }
+        if(Math.abs(this.speed)< this.friction){
+            this.speed = 0;
+        }
+        if(this.speed != 0){
+            const flip = this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle += 0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle -= 0.03*flip;
+            }
+        }
+       
+        this.x -= Math.sin(this.angle)*this.speed;
+        this.y -= Math.cos(this.angle)*this.speed;
+    }
     draw(ctx){
+
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2, 
-            this.y-this.height/2,
+            -this.width/2, 
+            -this.height/2,
             this.width,
             this.height
 
         );
-        ctx.fill()
+        ctx.fill();
+        ctx.restore();
     }
 }
